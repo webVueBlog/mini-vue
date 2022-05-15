daae89c963f8685d8b9a138b1c4be5d5b74a5950
webVueBlog <2979986590@qq.com>
2022-05-15 18:12:15 +0800

    fix: --

--- coding/92.反转链表-ii.js
+++ coding/92.反转链表-ii.js
@@ -18,26 +18,27 @@
  * @param {number} right
  * @return {ListNode}
  */
- var reverseBetween = function(head, left, right) {
-   let start = head, cur = head;
-   let i = 1;
-   while(i < left) {
-    start = cur;
-    cur = cur.next;
-    i++;
-   }
-   let prev = null, tail = cur; // tail截尾
-   while(i <= right) {
-    let next = cur.next;
-    cur.next = prev;
-    prev = cur;
-    cur = next;
-    i++;
-   }
-   start.next = prev;
-   tail.next = cur;
-   return left == 1 ? prev : head;
-  };
+
+var reverseBetween = function(head, left, right) {
+  let start = head, cur = head;
+  let i = 1;
+  while(i < left) {
+    start = cur;
+    cur = cur.next;
+    i++;
+  }
+  let prev = null, tail = cur; // tail截尾
+  while(i <= right) {
+    let next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+    i++;
+  }
+  start.next = prev;
+  tail.next = cur;
+  return left == 1 ? prev : head;
+};
   
 // @lc code=end
 
