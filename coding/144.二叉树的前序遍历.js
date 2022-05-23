7f1dd842734918f9d0e74339280b25d6b4bba8fc
webVueBlog <2979986590@qq.com>
2022-05-24 07:38:24 +0800

    fix:--

--- coding/144.二叉树的前序遍历.js
+++ coding/144.二叉树的前序遍历.js
@@ -19,11 +19,11 @@
  * (60 ms)
  */
 
- var preorderTraversal = function(root) {
+var preorderTraversal = function(root) {
   if (!root) return [];
   var result = [];
   var stack = [root];
-  
+
   while(stack.length) {
     var node = stack.pop();
     result.push(node.val);
