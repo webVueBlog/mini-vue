63af9c8cc722c9f089542330697b6afdc1a5179a
webVueBlog <2979986590@qq.com>
2022-05-25 18:04:02 +0800

    fix：--

---
+++ coding/129.求根节点到叶节点数字之和.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=javascript
+ *
+ * [129] 求根节点到叶节点数字之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+
+输入：root = [4,9,0,5,1]
+输出：1026
+解释：
+从根到叶子节点路径 4->9->5 代表数字 495
+从根到叶子节点路径 4->9->1 代表数字 491
+从根到叶子节点路径 4->0 代表数字 40
+因此，数字总和 = 495 + 491 + 40 = 1026
+
+输入：root = [1,2,3]
+输出：25
+解释：
+从根到叶子节点路径 1->2 代表数字 12
+从根到叶子节点路径 1->3 代表数字 13
+因此，数字总和 = 12 + 13 = 25
+
+ */
+var sumNumbers = function(root) {
+ function traverse(node, num) {
+  if(!node) return null;
+  num += node.val
+  if(!node.left && !node.right) return +num;
+  return traverse(node.left, num) + traverse(node.right, num);
+ }
+ return traverse(root, '');
+};
+// @lc code=end
+
