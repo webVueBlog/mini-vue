40459f5b62ec468effaba143e5e5899884ec5721
webVueBlog <2979986590@qq.com>
2022-06-03 01:05:45 +0800

    fix

---
+++ coding/473.火柴拼正方形.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=473 lang=javascript
+ *
+ * [473] 火柴拼正方形
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+var makesquare = function(matchsticks) {
+
+};
+// @lc code=end
+
