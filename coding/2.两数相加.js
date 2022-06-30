84b020aea67fca11431e7176ba64be95aefebdcb
webVueBlog <2979986590@qq.com>
2022-06-30 23:31:46 +0800

    fix: ✨ leetcode

--- coding/2.两数相加.js
+++ coding/2.两数相加.js
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode.cn id=2 lang=javascript
- *
- * [2] 两数相加
- */
-
-// @lc code=start
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -16,6 +9,32 @@
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
+ 方法：初等数学
+ 我们使用变量来跟踪进位，并从包含最低有效位的表头开始模拟逐位相加的过程
+ 对两数相加方法的可视化：342+465=807，每个结点都包含一个数字，并且数字按位逆序存储
+ 
+    l1 = 3 -> 4 -> 2 -> null
+    l2 = 4 -> 6 -> 5 -> null
+result = 7 -> 0 -> 8
+ carry = 0
+ 
+ 将当前结点初始化为返回列表的哑结点
+ 将进位carry初始化为0
+ 遍历列表l1和l2直至到达它们的尾端
+ 若达到l1末尾，则只计算l2
+ 若达到l2末尾，则只计算l1
+ 设定sum = x + y + carry
+ 创建一个数值为sum%10的新结点，并将其设置为当前结点的下一个结点，然后将当前结点前进到下一个结点。
+ 更新进位的值，carry为sum的十位
+ 将l1和l2前进到下一个节点
+ 检查carry = 1是否成立，如果成立，则向返回列表追加一个含有数字1的新结点
+ 
+ 复杂度分析
+
+时间复杂度：O(\max(m,n))O(max(m,n))，其中 mm 和 nn 分别为两个链表的长度。我们要遍历两个链表的全部位置，而处理每个位置只需要 O(1)O(1) 的时间。
+
+空间复杂度：O(1)O(1)。注意返回值不计入空间复杂度。
+ 
  */
 var addTwoNumbers = function(l1, l2) {
     const dummy = new ListNode();
@@ -36,5 +55,3 @@ var addTwoNumbers = function(l1, l2) {
     
     return dummy.next;
 };
-// @lc code=end
-
--- coding/3.无重复字符的最长子串.js
+++ coding/3.无重复字符的最长子串.js
@@ -8,13 +8,32 @@
 /**
  * @param {string} s
  * @return {number}
+ 方法一：暴力求解
+ 逐个生成子字符串
+ 看它是否不含有重复的字符
+ 
+ 方法二：滑动窗口及优化
+ 关键字：重复字符 -> 出现1次
+ 模式识别1:一旦涉及出现次数，需要用到散列表
+ 构造子串，散列表下标
+ 模式识别2:涉及子串，考虑滑动窗口
+ 
+ 举例分析思路
+ p w w k e w
+ 
+ 测试：
+ 字符串为空的情况
+ 字符串均为重复字符的情况
+ 测试其他常规输入
+ 
  */
- var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(s) {
     const set = new Set();
-    let [left, right, maxLen] = [0, 0, 0];
+    let [start, end, maxLen] = [0, 0, 0];
     
-    while (right < s.length) {
-        set.has(s[right]) ? set.delete(s[left++]) : set.add(s[right++]);
+    while(end < s.length) {
+        set.has(s[end]) ? set.delete(s[start++]) : set.add(s[end++]);
+        
         maxLen = Math.max(maxLen, set.size);
     }
     
